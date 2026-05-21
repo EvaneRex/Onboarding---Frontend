368585af24d9a91658261a9853a995bbb7dfc472
Signeg12 <siggre01@iba.dk>
2026-05-21 10:10:34 +0200

    tilføjet admindashboard

--- Onboarding/src/Private/adminDashboard.vue
+++ Onboarding/src/Private/adminDashboard.vue
@@ -1,3 +1,178 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<template></template>
+// Shared components
+import Header
+from '@/shared/header.vue'
+
+// Admin components
+import materialOverview
+from '@/components/admin/materialOverview.vue'
+
+import companyList
+from '@/components/admin/companyList.vue'
+
+import surveyOverview
+from '@/components/admin/surveyOverview.vue'
+
+// Styrer hvilken side der vises
+const currentView =
+  ref('dashboard')
+
+// Sender logout til App.vue
+const emit =
+  defineEmits([
+    'logout'
+  ])
+
+// Navigation
+function navigateTo(
+  view
+) {
+
+  currentView.value =
+    view
+}
+
+// Tilbage til dashboard
+function goBack() {
+
+  currentView.value =
+    'dashboard'
+}
+</script>
+
+<template>
+
+  <!-- DASHBOARD -->
+  <section
+    v-if="
+      currentView ===
+      'dashboard'
+    "
+    class="dashboard"
+  >
+
+    <!-- Header -->
+    <Header
+      @logout="
+        emit(
+          'logout'
+        )
+      "
+    />
+
+    <!-- Welcome -->
+    <section
+      class="welcome"
+    >
+
+      <h1>
+        Velkommen
+      </h1>
+
+      <p>
+        Her kan du tilgå
+        håndtering af
+        materialer såsom
+        upload eller
+        sletning eller få
+        overblik over
+        virksomheder/kunder
+        og derfra tildele
+        relevant materiale.
+      </p>
+
+    </section>
+
+    <!-- Dashboard cards -->
+    <section
+      class="dashboardCards"
+    >
+
+      <!-- MATERIALER -->
+      <div
+        class="card"
+        @click="
+          navigateTo(
+            'materials'
+          )
+        "
+      >
+
+        <h2>
+          Materialer
+        </h2>
+
+      </div>
+
+      <!-- SURVEY -->
+      <div
+        class="card"
+        @click="
+          navigateTo(
+            'survey'
+          )
+        "
+      >
+
+        <h2>
+          Spørgeskema
+        </h2>
+
+      </div>
+
+      <!-- KUNDER -->
+      <div
+        class="card"
+        @click="
+          navigateTo(
+            'clients'
+          )
+        "
+      >
+
+        <h2>
+          Kunder
+        </h2>
+
+      </div>
+
+    </section>
+
+  </section>
+
+  <!-- MATERIALER -->
+  <materialOverview
+    v-if="
+      currentView ===
+      'materials'
+    "
+    @goBack="
+      goBack
+    "
+  />
+
+  <!-- SURVEY -->
+  <surveyOverview
+    v-if="
+      currentView ===
+      'survey'
+    "
+    @goBack="
+      goBack
+    "
+  />
+
+  <!-- KUNDER -->
+  <companyList
+    v-if="
+      currentView ===
+      'clients'
+    "
+    @goBack="
+      goBack
+    "
+  />
+
+</template>
