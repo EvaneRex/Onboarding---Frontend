9b4aa902028236d7e922a9fd45ef41655c61636c
Signeg12 <siggre01@iba.dk>
2026-05-21 13:53:09 +0200

    clientdashboard + materiale oversigt

--- Onboarding/src/components/client/clientMatView.vue
+++ Onboarding/src/components/client/clientMatView.vue
@@ -1,9 +1,54 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  material: Object
+})
+
+const emit = defineEmits([
+  'goBack',
+  'complete'
+])
+
+const completeMaterial = () => {
+  emit('complete', props.material.index)
+}
+</script>
+
 <template>
-  <div>
-    <span class="icon">➜</span>
-    <p>Tilbage til dashboard</p>
-  </div>
-  <h1></h1>
-  <iframe></iframe>
+  <section class="materialView">
+    
+    <button
+      class="backBtn"
+      @click="emit('goBack')"
+    >
+      ← Tilbage
+    </button>
+
+    <div class="content">
+
+      <!-- YOUTUBE -->
+      <iframe
+        v-if="material.type === 'youtube'"
+        :src="material.src"
+        frameborder="0"
+        allowfullscreen
+        class="viewer"
+      />
+
+      <!-- PDF -->
+      <iframe
+        v-if="material.type === 'pdf'"
+        :src="material.src"
+        class="viewer"
+      />
+
+    </div>
+
+    <button
+      class="completeBtn"
+      @click="completeMaterial"
+    >
+      Marker som gennemført
+    </button>
+
+  </section>
 </template>
