49de25fa4a954ff088241ab8e224c86801c55ef0
Rikke Rasmussen <RiMaRa@outlook.dk>
2026-05-21 09:48:17 +0200

    uploadfile

--- Onboarding/src/components/admin/uploadFile.vue
+++ Onboarding/src/components/admin/uploadFile.vue
@@ -1,7 +1,46 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const filInput = ref(null)
+
+async function uploadPdfFiler() {
+  const filer = filInput.value.files
+  const formData = new FormData()
+
+  for (let i = 0; i < filer.length; i++) {
+    formData.append('filer', filer[i])
+  }
+
+  try {
+    const API_URL = 'http://localhost:2000'
+    const res = await fetch(`${API_URL}/onboarding/pdf-slides`, {
+      method: 'POST',
+      credentials: 'include',
+      body: formData,
+    })
+
+    const data = await res.json()
+
+    if (!res.ok) {
+      return console.error(data)
+    }
+
+    console.log(data)
+  } catch (err) {
+    console.error(err)
+  }
+}
+</script>
 <template>
   <div>
     <span class="icon">➜</span>
     <p>Tilbage til dashboard</p>
   </div>
+  <main>
+    <form @submit.prevent="uploadPdfFiler">
+      <label for="input">Vælg PDF-fil(er):</label>
+      <input id="input" ref="filInput" type="file" accept="application/pdf" multiple required />
+      <button type="submit">Upload</button>
+    </form>
+  </main>
 </template>
