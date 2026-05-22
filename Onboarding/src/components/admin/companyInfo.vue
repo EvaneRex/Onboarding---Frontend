c205bc004a04bb966e48430879fb8320a7917f29
Signeg12 <siggre01@iba.dk>
2026-05-22 11:40:29 +0200

    imit

--- Onboarding/src/components/admin/companyInfo.vue
+++ Onboarding/src/components/admin/companyInfo.vue
@@ -1,12 +1,9 @@
 <script setup>
-import Header from '@/components/shared/header.vue'
+
 const emit = defineEmits(['goBack'])
 
-defineProps({
-  logout: Function,
-})
+
 </script>
 <template>
-  <Header :logout="logout" />
   <button class="backBtn" @click="emit('goBack')">⟵ Tilbage til dashboard</button>
 </template>
