d8e1c86150cfa9061804951abedb901a8bbd2ded
Rikke Rasmussen <RiMaRa@outlook.dk>
2026-05-22 14:06:52 +0200

    fix - ny info fra backend

--- Onboarding/src/components/admin/uploadVideo.vue
+++ Onboarding/src/components/admin/uploadVideo.vue
@@ -1,11 +1,20 @@
 <script setup>
 import { ref } from 'vue'
 
-const titel = ref('')
-const url = ref('')
+const links = ref([{ titel: '', url: '' }])
+
+function tilføjLink() {
+  links.value.push({ titel: '', url: '' })
+}
+
+function fjernLink(index) {
+  if (links.value.length > 1) {
+    links.value.splice(index, 1)
+  }
+}
 
 async function uploadVideo() {
-  if (!titel.value || !url.value) {
+  if (links.value.some((link) => !link.titel || !link.url)) {
     alert('Udfyld alle felter')
     return
   }
@@ -16,7 +25,7 @@ async function uploadVideo() {
       method: 'POST',
       credentials: 'include',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify([{ titel: titel.value, url: url.value }]),
+      body: JSON.stringify(links.value),
     })
 
     const data = await res.json()
@@ -26,9 +35,8 @@ async function uploadVideo() {
       return
     }
 
-    alert('YouTube-link uploadet!')
-    titel.value = ''
-    url.value = ''
+    alert('YouTube-links uploadet!')
+    links.value = [{ titel: '', url: '' }]
   } catch (err) {
     alert('Der opstod en fejl.')
     console.error(err)
@@ -37,15 +45,23 @@ async function uploadVideo() {
 </script>
 
 <template>
-  <form @submit.prevent="uploadVideo">
-    <label>
-      Titel:
-      <input v-model="titel" type="text" required />
-    </label>
-    <label>
-      YouTube-link:
-      <input v-model="url" type="url" required />
-    </label>
-    <button type="submit">Upload video link</button>
+  <form id="dynamicForm" @submit.prevent="uploadVideo">
+    <div class="formGruppe" v-for="(link, idx) in links" :key="idx" style="margin-bottom: 1em">
+      <label>
+        Titel:
+        <input v-model="link.titel" type="text" required />
+      </label>
+      <label>
+        YouTube-link:
+        <input v-model="link.url" type="url" required />
+      </label>
+      <button type="button" @click="fjernLink(idx)" v-if="links.length > 1">
+        <img class="trash" src="@/assets/icon/trash-solid-full.svg" />
+      </button>
+    </div>
+    <div class="formGruppe">
+      <button type="button" @click="tilføjLink">Tilføj flere</button>
+    </div>
+    <button type="submit">Upload video links</button>
   </form>
 </template>
